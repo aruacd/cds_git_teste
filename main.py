caf18452e6d1391416e8c787ead8d78656c2abaa
Aruã Dias <aruacd@gmail.com>
2023-07-17 19:41:31 -0300

    Adicionado funcao de coleta de dados

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def main():
-    print(2*2)
-    return none
+def gather_data():
+    n1 = int(input("Primeiro valor: "))
+    n2 = int(input("Segundo valor: "))
 
+    return n1, n2
+
+def main():
+    n1, n2 = gather_data()
+
+    print(n1*n2)
+
+    return none
 
 if __name__ == "__main__":
     main()
